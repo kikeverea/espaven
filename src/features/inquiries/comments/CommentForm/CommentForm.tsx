84c8ab494d12de4269ef1d3680128a96c93a446e
Enrique Verea <kikeverea@gmail.com>
2026-08-21 13:53:55 +0200

    Refactor inquiry comments form

--- src/features/inquiries/comments/CommentForm/CommentForm.tsx
+++ src/features/inquiries/comments/CommentForm/CommentForm.tsx
@@ -1,16 +1,8 @@
-'use client'
-
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 
-import { Button } from '@/components/ui/button.tsx'
-import { Spinner } from '@/components/ui/spinner.tsx'
 import type { Inquiry, InquiryComment, NewInquiryComment } from '@/features/types.ts'
-import { formSchema, applyData } from '@/features/inquiries/comments/CommentForm/schema.ts'
 import { useInquiryCommentsMutations } from '@/features/inquiries/comments/useInquiryComments'
-import { toast } from "@/components/ui/toast.tsx"
-import FormTextarea from '@/components/Form/FormTextarea.tsx'
+import Form, { type InferFormData } from '@/components/Form/Form.tsx'
 
 type CommentFormProps = {
   inquiry: Inquiry
@@ -18,52 +10,33 @@ type CommentFormProps = {
   onCancel: () => void
 }
 
-const CommentForm = ({ inquiry, comment, onCancel }: CommentFormProps) => {
-  const { create, update, status } = useInquiryCommentsMutations(inquiry)
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    disabled: status.pending.any
-  })
-
-  const handleSubmit = (formData: z.infer<typeof formSchema>) => {
-    if (comment === null) return
-
-    const submitInquiryComment = applyData(inquiry, comment, formData)
-
-    const onSuccess = () => {
-      toast.add({ title: "Comentario guardado" })
-      form.reset()
+export const config = {
+  fields: {
+    body: {
+      schema: z.string().min(2, 'Mínimo 2 caracteres').max(500, 'Máximo 500 caracteres'),
     }
-
-    if ('id' in submitInquiryComment)
-      update(submitInquiryComment, { onSuccess })
-    else
-      create(submitInquiryComment, { onSuccess })
   }
+}
 
-  const handleCancel = () => {
-    form.reset()
-    onCancel()
-  }
+export const applyData = (
+  comment: NewInquiryComment | InquiryComment,
+  formData: InferFormData<typeof config.fields>
+): InquiryComment | NewInquiryComment => {
+
+  return { inquiry: comment.inquiry, ...comment, ...formData }
+}
+
+const CommentForm = ({ inquiry, comment, onCancel }: CommentFormProps) => {
 
   return (
-    <>
-      <form id='inquiry-comments-form' onSubmit={form.handleSubmit(handleSubmit)} >
-        <FormTextarea form={ form } name='body' placeholder='Nuevo comentario' maxLength={ 500 }/>
-
-        <div className='flex gap-2'>
-          <Button type='button' variant='outline' onClick={ handleCancel } disabled={ status.pending.any }>
-            Cancelar
-          </Button>
-          <Button type='submit' form='inquiry-comments-form' disabled={ status.pending.any }>
-            Guardar
-            { status.pending.any && <Spinner />}
-          </Button>
-        </div>
-      </form>
-
-    </>
+    <Form
+      name='inquiry-comment'
+      config={ config }
+      item={{ ...comment, inquiry }}
+      mutations={useInquiryCommentsMutations(inquiry)}
+      applyData={applyData}
+      onCancel={onCancel}
+    />
   )
 }
 
--- src/features/inquiries/comments/CommentForm/schema.test.ts
+++ src/features/inquiries/comments/CommentForm/schema.test.ts
@@ -1,64 +1,21 @@
 import * as z from 'zod'
-import { formSchema } from '@/features/inquiries/InquiryForm/schema.ts'
+import { config } from '@/features/inquiries/comments/CommentForm/CommentForm'
+import { extractSchema } from '@/components/Form/Form.tsx'
+const schema = extractSchema(config)
 
-describe('inquiryFormSchema', () => {
-  
+describe('inquiryCommentFormSchema', () => {
+
   test('accepts a valid inquiry', () => {
-    const result = formSchema.safeParse(inquiry())
+    const result = schema.safeParse(inquiryComment())
     expect(result.success).toBe(true)
   })
 
-  it.each([
-    'name',
-    'lastName',
-    'service',
-  ])('rejects if %s not present', field => {
-    const result = formSchema.safeParse(inquiry({ [field]: null }))
+  it.each([ 'body' ])('rejects if %s not present', field => {
+    const result = schema.safeParse(inquiryComment({ [field]: null }))
     expect(result.success).toBe(false)
-  })
-
-  it.each([
-    'name',
-    'lastName',
-    'service',
-  ])('rejects if %s length less than min length, passes if not', field => {
-    const invalid = formSchema.safeParse(inquiry({ [field]: '1' }))
-    const valid = formSchema.safeParse(inquiry({ [field]: '11' }))
-
-    expect(valid.success).toBe(true)
-    expect(invalid.success).toBe(false)
-  })
-
-  it.each([
-    ['name', 48],
-    ['lastName', 48],
-    ['service', 250],
-  ])('rejects if %s length more than max length, passes if not', (field, maxLength) => {
-    const valid = formSchema.safeParse(inquiry({ [field]: '1'.repeat(maxLength) }))
-    const invalid = formSchema.safeParse(inquiry({ [field]: '1'.repeat(maxLength + 1) }))
-
-    expect(valid.success).toBe(true)
-    expect(invalid.success).toBe(false)
-  })
-
-  test('inquiry without email but with phone number is valid', () => {
-    const result = formSchema.safeParse(inquiry({ emails: []}))
-    expect(result.success).toBe(true)
-  })
-
-  test('inquiry without phone numbers but with emails is valid', () => {
-    const result = formSchema.safeParse(inquiry({ phoneNumbers: []}))
-    expect(result.success).toBe(true)
   })
 })
 
-const inquiry = (customFields: Partial<z.infer<typeof formSchema>> = {}) => {
-  return {
-    name: 'Name',
-    lastName: 'Last Name',
-    service: 'Service',
-    emails: [{ value: "test@mail.com" }],
-    phoneNumbers: [{ value: "555 555 555" }],
-    ...customFields
-  }
+const inquiryComment = (customFields: Partial<z.infer<typeof schema>> = {}) => {
+  return { body: 'A body', ...customFields }
 }
